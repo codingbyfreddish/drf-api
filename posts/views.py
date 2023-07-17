d17902861b846c9b3f99e5c46aa9aa5e6473365e
Fredrik Gustafsson <freddish@hotmail.com>
2023-07-17 17:43:51 +0000

    added JSON Web Tokens to the project and configured them

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,9 @@
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
+from django_filters.rest_framework import DjangoFilterBackend
 from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Post
 from .serializers import PostSerializer
-from django_filters.rest_framework import DjangoFilterBackend
 
 
 class PostList(generics.ListCreateAPIView):
