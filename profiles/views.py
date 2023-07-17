d190a322e8b5a1484a769a50f75c37409037e7f6
Fredrik Gustafsson <freddish@hotmail.com>
2023-07-17 13:52:03 +0000

    added serializers.py, added ProfileSerializer class. imported ProfileSerializer to profiles/views.py, added get func to views

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,14 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Profile
+from .serializers import ProfileSerializer
+
+
+class ProfileList(APIView):
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
+
+    def get(self, request):
+        serializer = ProfileSerializer(profiles, many=True)
+        return Response(serializer.data)
