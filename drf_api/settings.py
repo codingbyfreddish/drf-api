d9167e31370a6f55c6926d4bd561d622eaae59b5
Fredrik Gustafsson <freddish@hotmail.com>
2023-07-18 07:47:16 +0000

    added CORS_ALLOW_CREDENTIALS = True to settings.py

--- drf_api/settings.py
+++ drf_api/settings.py
@@ -118,6 +118,8 @@ if 'CLIENT_ORIGIN_DEV' in os.environ:
         rf"{extracted_url}(eu|us)\d+\w\.gitpod\.io$",
     ]
 
+CORS_ALLOW_CREDENTIALS = True
+
 ROOT_URLCONF = 'drf_api.urls'
 
 TEMPLATES = [
